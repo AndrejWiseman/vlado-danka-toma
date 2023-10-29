ac235c3b38bdbf71b833ab531ba2fc5a492490d6
A.M. <andrej2000aa@gmail.com>
2023-10-29 13:57:06 +0100

    ppp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
+import Kartica from "./Card";
+
+// import Button from 'react-bootstrap/Button';
+
+import { MDBBtn } from 'mdb-react-ui-kit';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className="box">
+        <h1>Za Vladu i Danku :)</h1>
+        <h2>Film: Toma</h2>
+
+        {/*<Button variant="primary">Primary</Button>*/}
+        {/*<Button variant="info">Info</Button>*/}
+
+        <div className="btn-box">
+
+          <a href="#">
+            <MDBBtn className='me-1' color='warning'>
+              Preuzmi film
+            </MDBBtn>
+          </a>
+
+          <a href="#">
+            <MDBBtn className='me-1' color='warning'>
+              Gledaj Online
+            </MDBBtn>
+          </a>
+        </div>
+
+
+
+
+      </div>
+
     </div>
   );
 }
---
+++ src/Button.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Button = () => {
+  return(
+      <>
+        <h2>Dugme</h2>
+      </>
+  )
+}
+export default Button
